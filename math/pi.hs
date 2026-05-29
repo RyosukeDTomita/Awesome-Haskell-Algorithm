338dd7dccf849442886bb0bdf79102db4f7ed522
RyosukeDTomita <d310151c2@gmail.com>
2026-05-29 12:58:30 +0900

    chore: add comment

--- math/pi.hs
+++ math/pi.hs
@@ -1,7 +1,12 @@
--- 円周率piが3.14以上であることがわかるn角形をもとめる
+-- 円周率piが3.14以上であることがわかるn角形をもとめる(多角形近似)
 import Text.Printf (printf)
 
--- n角形による π の近似
+-- n角形によるpiの近似
+-- 半径 1 の円の円周は2 * pi
+-- 内接正 n 角形の周長はn * 2 * sin(pi / n)
+-- 2 * pi ≒ n * 2 * sin(pi / n)
+-- 両辺を 2 で割ると、
+-- pi ≒ n * sin(pi / n)
 approxPi :: Int -> Double
 approxPi n
   | n >= 3 = fromIntegral n * sin (pi / fromIntegral n)
