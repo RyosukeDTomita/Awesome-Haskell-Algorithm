55b4fe6e37557f0b7147769081d653f781106306
RyosukeDTomita <d310151c2@gmail.com>
2026-06-10 11:26:40 +0900

    chore ghc9122

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Haskell dev environment AtCoder";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -23,15 +23,14 @@
         devShells.default = pkgs.mkShell {
           packages = [
             pkgs.zsh
-            (pkgs.haskell.packages.ghc96.ghcWithPackages (ps: [
+            (pkgs.haskell.packages.ghc9122.ghcWithPackages (ps: [
               ps.vector
               ps.containers
               ps.bytestring
               ps.unordered-containers
               ps.hashable
             ]))
-            pkgs.haskell.packages.ghc96.cabal-install
-            pkgs.haskell.packages.ghc96.haskell-language-server
+            pkgs.haskell.packages.ghc9122.haskell-language-server
           ];
         };
       }
