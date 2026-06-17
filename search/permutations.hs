8bd74a8a607feca2dc3566fa63dcffdfd8e0ee1b
RyosukeDTomita <d310151c2@gmail.com>
2026-06-17 12:28:40 +0900

    feat: permutaions

--- search/permutations.hs
+++ search/permutations.hs
@@ -1,16 +1,24 @@
-permutations' :: [a] -> [[a]]
+import Debug.Trace (traceShowId)
+
+permutations' :: (Show a) => [a] -> [[a]]
 permutations' [] = [[]]
 permutations' xs =
   [ x : p
   | (x, rest) <- selections xs,
-    p <- permutations' rest
+    p <- permutations' rest -- 再帰でrestの中での並び替えを実施する
   ]
   where
-    -- 各要素を1つ取り出し、(取り出した要素, 残り) を全通り返す
-    selections :: [a] -> [(a, [a])]
+    -- | 先頭に来る要素を1つ選び、残りをそのままの順で返す
+    -- [1, 2, 3]の場合 [ (1,[2,3]), (2,[1,3]), (3,[1,2]) ]
+    selections :: (Show a) => [a] -> [(a, [a])]
     selections [] = []
     selections (y : ys) =
-      (y, ys) : [(z, y : zs) | (z, zs) <- selections ys]
+      -- traceShowId $ (y, ys)
+      (y, ys)
+        : [ (z, y : zs)
+          | (z, zs) <- selections ys
+          ]
 
 main :: IO ()
-main = print $ permutations' [1, 2, 3 :: Int]
+main = do
+  print $ permutations' [1, 2, 3]
