f318cd719d3a0a747a4c7b9c1563391d7fca2f2e
RyosukeDTomita <d310151c2@gmail.com>
2026-07-15 08:50:19 +0900

    faet: monoid fold

--- data_structure/tree/fingerTree/fold.hs
+++ data_structure/tree/fingerTree/fold.hs
@@ -1,8 +1,24 @@
-import Data.Foldable (fold)
+{-# LANGUAGE MonoLocalBinds #-}
+{-# OPTIONS_GHC -Wunused-imports #-}
+
 import Data.Monoid (Sum (..))
+import Prelude hiding (foldMap, mappend, mconcat)
+
+-- | すでに要素がMonoidの列をそのまま畳み込む
+fold :: (Foldable t, Monoid m) => t m -> m
+fold xs = foldMap id xs
+
+-- | monoid版fold
+-- memptyは単位元
+foldMap :: (Foldable t, Monoid m) => (a -> m) -> t a -> m
+foldMap f xs = foldr (\a acc -> mappend (f a) acc) mempty xs
+
+-- | <>は2つの同じ型の値を一つにまとめる演算
+mappend :: (Semigroup a) => a -> a -> a
+mappend a b = a <> b
 
 main :: IO ()
 main = do
-  -- foldはfoldMapでできていて、foldMapはfoldrでできてそうに見える
-  let sumList = fold [1, 2, 3 :: Sum Int]
+  let sumList = fold [1, 2, 3 :: Sum Int] -- リストがMonoidになるように型を付与
   print sumList -- Sum {getSum = 6}
+  print $ foldMap Sum [1, 2, 3]
